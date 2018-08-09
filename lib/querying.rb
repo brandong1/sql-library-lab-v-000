40b4b14d449a8d6cf5ccdae8c8198cf217d62bee
Brandon Green <brandon@brandonleeproductions.com>
2018-08-09 01:59:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species)
   FROM characters
-  WHERE characters.species = COUNT(species)
+  WHERE characters.species =
    "
 end
 
