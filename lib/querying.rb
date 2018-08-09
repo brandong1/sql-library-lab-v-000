062768d757dd0e9691e1061713f5ea16b06618a5
Brandon Green <brandon@brandonleeproductions.com>
2018-08-09 01:56:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+  FROM characters
+  WHERE characters.species = COUNT(species)
+   "
 end
 
 def select_name_and_series_subgenres_of_authors
